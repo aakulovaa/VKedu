bd01f35ecd360f9ce5a09c75729446661464f544
annakulova <aakulovaa03@gmail.com>
2026-03-12 00:29:03 +0300

    VKEdu: создала header и поменяла иконку приложения

--- app/src/main/java/com/aakulovaa/vkedu/MainActivity.kt
+++ app/src/main/java/com/aakulovaa/vkedu/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.aakulovaa.vkedu.ui.screens.AppListScreen
 import com.aakulovaa.vkedu.ui.theme.VKeduTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             VKeduTheme {
-
+                AppListScreen()
             }
         }
     }
--- app/src/main/java/com/aakulovaa/vkedu/ui/theme/Theme.kt
+++ app/src/main/java/com/aakulovaa/vkedu/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.aakulovaa.vkedu.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
